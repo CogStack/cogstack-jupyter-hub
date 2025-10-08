fa96139b7309ce256508a4564af40288723ca50c
vladd-bit <vlad.a.dinu@gmail.com>
2025-10-08 12:40:18 +0100

    Updated cert/env update script.

--- scripts/update_env_cert_from_nifi_repo.sh
+++ scripts/update_env_cert_from_nifi_repo.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
-curl https://raw.githubusercontent.com/CogStack/CogStack-NiFi/refs/heads/main/deploy/general.env > ./env/general.env
-curl https://raw.githubusercontent.com/CogStack/CogStack-NiFi/refs/heads/main/security/root-ca.key > ./security/root-ca.key
-curl https://raw.githubusercontent.com/CogStack/CogStack-NiFi/refs/heads/main/security/root-ca.pem > ./security/root-ca.pem
+CURRENT_DIR=$(basename "$PWD")
+
+OUTPUT_DIR_PATH_PREFIX="../"
+# If we are in a "scripts" subdirectory that has a parent containing common project markers
+if [[ "$CURRENT_DIR" == "scripts" && ( -f ../Makefile || -f ../README.* || -d ../src || -d ../app ) ]]; then
+    OUTPUT_DIR_PATH_PREFIX="../"
+# If we are in a project root (heuristic: has a scripts/ folder or common project files)
+elif [[ -d scripts || -f Makefile || -f README.* || -d src || -d app ]]; then
+    OUTPUT_DIR_PATH_PREFIX="./"
+fi
+
+curl https://raw.githubusercontent.com/CogStack/CogStack-NiFi/refs/heads/main/deploy/general.env > ${OUTPUT_DIR_PATH_PREFIX}env/general.env
+curl https://raw.githubusercontent.com/CogStack/CogStack-NiFi/refs/heads/main/security/certificates/root/root-ca.key > ${OUTPUT_DIR_PATH_PREFIX}security/root-ca.key
+curl https://raw.githubusercontent.com/CogStack/CogStack-NiFi/refs/heads/main/security/certificates/root/root-ca.pem > ${OUTPUT_DIR_PATH_PREFIX}security/root-ca.pem
